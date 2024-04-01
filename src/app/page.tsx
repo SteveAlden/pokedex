a44c6b819e4e3bc740279c58788204ab4ddf0fb6
SteveAlden <stevealden.77@gmail.com>
2024-04-01 23:01:51 +0530

    Update API limit to fetch 151 pokemons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import styles from './page.module.css';
 import { PokemonList } from '@/components/PokemonList';
 
 const fetctPokemons = async () => {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100');
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
   const pokemonData = await res.json();
   return { pokemon: pokemonData.results };
 };

84854bd9884202a40bf02d5cbf3792d13a57021a
SteveAlden <stevealden.77@gmail.com>
2024-04-04 23:32:39 +0530

    Refactor PokemonList component and add typography to ImageListItemBar

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { Masonry } from '@mui/lab';
-import { ImageListItem, ImageListItemBar } from '@mui/material';
+import { ImageListItem, ImageListItemBar, Typography } from '@mui/material';
 import Link from 'next/link';
 interface Pokemon {
   name: string;
@@ -18,40 +18,55 @@ interface PokemonListItemProps {
 
 export const PokemonList: React.FC<PokemonListProps> = ({ pokemon }) => {
   return (
-    <div>
-      <h1>Pokemon List</h1>
-      <Masonry spacing={2} columns={5}>
-        {pokemon.map((poke, index) => (
-          <PokemonListItem poke={poke} index={index} key={poke.name} />
-        ))}
-      </Masonry>
-    </div>
+    <Masonry spacing={1.2} columns={{ xs: 2, sm: 3, md: 4, lg: 5, xl: 6 }}>
+      {pokemon.map((poke, index) => (
+        <PokemonListItem poke={poke} index={index + 1} key={poke.name} />
+      ))}
+    </Masonry>
   );
 };
 
 const PokemonListItem: React.FC<PokemonListItemProps> = ({ poke, index }) => (
-  <Link href={`/pokemon/${poke.name}`} key={index}>
+  <Link href={`/pokemon/${index}`} key={index}>
     <ImageListItem
       key={index}
       sx={{
         borderRadius: '10px',
         paddingTop: '20px',
-        transition: '0.1s',
+        transition: '0.15s',
         backdropFilter: 'blur(16px) saturate(180%)',
         '-webkit-backdrop-filter': 'blur(16px) saturate(180%)',
-        backgroundColor: '#161616',
+        backgroundColor: 'rgb(35, 35, 35)',
         '&:hover': {
-          boxShadow: '0 0 10px #5356FF',
+          zIndex: 1,
+          transform: 'scale(1.05)',
+
+          boxShadow:
+            '0px 5px 10px 5px rgba(232,35,111,255), 0px 5px 10px 5px rgba(108, 28, 107,0.5)',
         },
       }}
     >
       <img
-        srcSet={`/images/pokemon/poke-${index + 1}.png?w=248&fit=crop&auto=format&dpr=2 2x`}
-        src={`/images/pokemon/poke-${index + 1}.png?w=248&fit=crop&auto=format`}
+        srcSet={`/images/pokemon-trim-top/poke-${index}.png?w=248&fit=crop&auto=format&dpr=2 2x`}
+        src={`/images/pokemon-trim-top/poke-${index}.png?w=248&fit=crop&auto=format`}
         alt={poke.name}
-        loading='lazy'
+        loading='eager'
+        style={{
+          WebkitBoxReflect:
+            'below 0px -webkit-gradient(linear, left top, left bottom, from(transparent), color-stop(70%, transparent) , to(rgba(250, 250, 250, 0.1)))',
+        }}
       />
-      <ImageListItemBar position='below' title={poke.name} />
+      <ImageListItemBar
+        position='below'
+        title={
+          <Typography sx={{ color: 'rgb(137, 137, 137)', textAlign: 'center' }}>
+            {poke.name?.toUpperCase()}
+          </Typography>
+        }
+        style={{ display: 'flex', justifyContent: 'center' }}
+      >
+        {poke.name}
+      </ImageListItemBar>
     </ImageListItem>
   </Link>
 );

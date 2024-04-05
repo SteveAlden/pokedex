b5e2db3336d88eaf479278d492bd5d54bc8b31cf
SteveAlden <stevealden.77@gmail.com>
2024-04-05 16:08:52 +0530

    Add Filled and Hollow wrapper components

---
+++ src/components/wrapper/Filled.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Box, Paper, Typography } from '@mui/material';
+
+const paperStyles = {
+  width: '100%',
+  margin: '5px',
+  backgroundColor: 'rgb(25, 25, 25)',
+  borderRadius: '12px',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  boxShadow: '0px 5px 10px 0px rgba(0, 0, 0, 0.4)',
+  padding: '2vh 5vw 2vh 5vw',
+  marginTop: '20px',
+};
+
+const Filled: React.FC<any> = ({ children }) => {
+  return <Paper sx={paperStyles}>{children}</Paper>;
+};
+
+export default Filled;
---
+++ src/components/wrapper/Hollow.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Paper, Typography } from '@mui/material';
+
+const paperStyles = {
+  padding: '2vh 5vw 2vh 5vw',
+  borderRadius: '12px',
+  border: '5px solid rgb(25, 25, 25)',
+  backgroundColor: 'rgb(20, 20, 20)',
+  boxShadow: '0px 5px 10px 0px rgba(0, 0, 0, 0.4)',
+  display: 'flex',
+  justifyContent: 'space-between',
+  width: '100%',
+  marginTop: '20px',
+};
+
+const Hollow: React.FC<any> = ({ children }) => {
+  return <Paper sx={paperStyles}>{children}</Paper>;
+};
+
+export default Hollow;

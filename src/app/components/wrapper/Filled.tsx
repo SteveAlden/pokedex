306c3d9cbb8fde551216919e4f25eb1720ca207a
SteveAlden <stevealden.77@gmail.com>
2024-04-09 22:49:14 +0530

    Update border radius and add margin bottom to wrapper components

--- src/app/components/wrapper/Filled.tsx
+++ src/app/components/wrapper/Filled.tsx
@@ -4,7 +4,7 @@ import { Box, Paper, Typography } from '@mui/material';
 const paperStyles = {
   width: '100%',
   backgroundColor: 'rgb(25, 25, 25)',
-  borderRadius: '12px',
+  borderRadius: '32px',
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
@@ -12,6 +12,7 @@ const paperStyles = {
   boxShadow: '0px 5px 10px 0px rgba(0, 0, 0, 0.4)',
   padding: '2vh 5vw 2vh 5vw',
   marginTop: '20px',
+  marginBottom: '20px',
 };
 
 const Filled: React.FC<any> = ({ children }) => {
--- src/app/components/wrapper/Hollow.tsx
+++ src/app/components/wrapper/Hollow.tsx
@@ -4,13 +4,14 @@ import { Paper, Typography } from '@mui/material';
 const paperStyles = {
   width: '100%',
   padding: '2vh 5vw 2vh 5vw',
-  borderRadius: '12px',
+  borderRadius: '32px',
   border: '5px solid rgb(25, 25, 25)',
   backgroundColor: 'rgb(20, 20, 20)',
   boxShadow: '0px 5px 10px 0px rgba(0, 0, 0, 0.4)',
   display: 'flex',
   justifyContent: 'space-between',
   marginTop: '20px',
+  marginBottom: '20px',
 };
 
 const Hollow: React.FC<any> = ({ children }) => {
